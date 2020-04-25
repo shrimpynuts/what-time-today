9e26c1baa466ef4bb3ee68bd7ad190464e4351ff
Jonathan Cai <caimjonathan@gmail.com>
2020-04-25 12:22:42 -0700

    little touch

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,6 +4,7 @@ export default function About() {
     return (
         <div className="not-homepage">
         <p>Created by <a style={{color: 'black'}} target="_blank" href="https://jonathancai.com/">Jonathan Cai</a>.</p>
+        <p>Big thanks to the <a style={{color: 'black'}} target="_blank" href="https://www.instagram.com/pablostanley/">creator</a> of <a style={{color: 'black'}} target="_blank" href="https://www.instagram.com/pablostanley/">Open Peeps</a>, the illustration library that this site was heavily inspired by. </p>
         <p>Code is <a style={{color: 'black'}} target="_blank" href="https://github.com/jonathancai11/what-time-today">here</a>.</p>
         <p>April 2020.</p>
         </div>
--- src/components/privacy/Privacy.js
+++ src/components/privacy/Privacy.js
@@ -5,9 +5,8 @@ export default function Privacy() {
         <div className="not-homepage">
             <p>Security and user data privacy is taken seriously.</p>
             <p>Signing in through Google allows access to your name, your email address, and your Google Calendar data.</p>
-            <b>All of the above are not stored.</b>
+            <b>All of the above are not stored in any database.</b>
             <p>Thus, no emails will be sent to your email address.</p>
-            {/* <p>Your Google Calendar data will only be retrieved if you decide to sign in. </p> */}
             <p>If you choose to sign in, your calendars are retrieved through the Google Calendar API and is only used to display on the home page.</p>
             <p>You may revoke access to your Google Calendar data by signing out, and the data will be removed from your session.</p>
             <p>The availabities that you select on the calendar only persist for your session and is not stored anywhere.</p>
