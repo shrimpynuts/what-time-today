ab56d1b35dd09e7170d53e20d41e0a83a9c75f58
Jonathan Cai <caimjonathan@gmail.com>
2020-04-29 13:54:32 -0700

    url

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css'
 export default function Footer() {
     return (
     <div className="footer">
-        <a className="privacy-link" target="_blank" href="https://forms.gle/E83Y2mXJeLs9zLuJ7">Feedback?</a>
+        <a className="privacy-link" rel="noopener noreferrer" target="_blank" href="https://forms.gle/E83Y2mXJeLs9zLuJ7">Feedback?</a>
         <Link className="privacy-link" to="/privacy">Privacy</Link>
         <Link className="privacy-link" to="/about">About</Link>
       </div>
