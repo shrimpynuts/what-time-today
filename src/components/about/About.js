cc236c0225f536ed57ba504a12b3d6dcd5fbc0cc
Jonathan Cai <caimjonathan@gmail.com>
2020-04-30 22:51:21 -0700

    readme and about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,7 +4,8 @@ export default function About() {
     return (
         <div className="not-homepage">
             <div className="aboutpage">
-                <p>Thanks to the <a style={{color: 'black'}} target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/pablostanley/">creator</a> of <a style={{color: 'black'}} target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/pablostanley/">Open Peeps</a>, the illustration library that this site was inspired by. </p>
+                <p>Thanks to <a style={{color: 'black'}} target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/pablostanley/">Pablo Stanley</a> for <a style={{color: 'black'}} target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/pablostanley/">Open Peeps</a>. </p>
+                <p>Thanks to <a style={{color: 'black'}} target="_blank" rel="noopener noreferrer" href="https://github.com/jquense">jquense</a> for <a style={{color: 'black'}} target="_blank" rel="noopener noreferrer" href="https://www.npmjs.com/package/react-big-calendar/">react-big-calendar</a>.</p>
                 <p>Made by <a style={{color: 'black'}} target="_blank" rel="noopener noreferrer" href="https://jonathancai.com/">Jonathan Cai</a>.</p>
                 <p>Code is <a style={{color: 'black'}} target="_blank" rel="noopener noreferrer" href="https://github.com/jonathancai11/what-time-today">here</a>.</p>
                 <p>April 2020.</p>
