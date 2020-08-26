3b2a5c6865e7d6ed9e076ed88db4a5178828c4dd
Jonathan Cai <caimjonathan@gmail.com>
2020-08-26 00:44:28 -0700

    refactoring plus new counter

--- src/App.js
+++ src/App.js
@@ -25,13 +25,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const moment = require('moment-timezone');
 
 var peep1 = require('./assets/peep1.png');
-var rand = Math.floor(Math.random() * 16) + 2; 
+var rand = Math.floor(Math.random() * 16) + 2;
 var peep2 = require('./assets/peep' + rand.toString() + '.png');
 
 
 var offset = Intl.DateTimeFormat().resolvedOptions().timeZone;
 var USTimeZones = ["America/Los_Angeles", "America/Denver", "America/Chicago", "America/New_York"];
-var WorldWideTimeZones = ["Asia/Kolkata", "Asia/Shanghai", "Asia/Hong_Kong", "Asia/Tokyo", 
+var WorldWideTimeZones = ["Asia/Kolkata", "Asia/Shanghai", "Asia/Hong_Kong", "Asia/Tokyo",
   "Australia/Sydney", "Australia/Darwin", "Europe/Paris", "Europe/Berlin", "Etc/GMT"]
 var AllTimeZones = [offset, ...USTimeZones.filter(tz => tz !== offset), ...WorldWideTimeZones.filter(tz => tz !== offset)].sort((a, b) => a > b);
 
@@ -43,23 +43,22 @@ function App() {
   const [MonthDay, setMonthDay] = useState(true);
   const [timeZone, setTimeZone] = useState(offset);
   const [timeZones, setTimeZones] = useState(AllTimeZones.filter((tz) => tz !== offset));
-  
+
   const [messageType, setMessageType] = useState(messageTypes[0]);
-  
+
   useEffect(() => {
     setTimeZones(AllTimeZones.filter((tz) => tz !== timeZone));
   }, [timeZone]);
-
 
   const dispatch = useDispatch();
   const { availabilities } = useSelector(getAvailabilities);
 
   const classes = makeStyles({
     card: {
-    borderRadius: 0,
-    backgroundColor: "grey",
-    color: "black",
-    boxShadow: "none"
+      borderRadius: 0,
+      backgroundColor: "grey",
+      color: "black",
+      boxShadow: "none"
     }
   });
 
@@ -75,14 +74,14 @@ function App() {
 
   const setUserCallback = (user) => {
     if (user) {
-        let userProfile = user.getBasicProfile();
-        let newUser = {
-          firstName: userProfile.getGivenName(),
-          lastName: userProfile.getFamilyName(),
-          email: userProfile.getEmail(),
-        }
-        dispatch(signIn(newUser));
-        getAndDisplayEvents(dispatch);
+      let userProfile = user.getBasicProfile();
+      let newUser = {
+        firstName: userProfile.getGivenName(),
+        lastName: userProfile.getFamilyName(),
+        email: userProfile.getEmail(),
+      }
+      dispatch(signIn(newUser));
+      getAndDisplayEvents(dispatch);
     } else {
       // dispatch(signOut());
     }
@@ -112,168 +111,174 @@ function App() {
 
   var width = window.innerWidth;
 
-  const toolbar = 
-  (
-  <div className="copytext">
-    {document.queryCommandSupported('copy') &&
-      <OverlayTrigger
-      placement={"top"}
-      overlay={
-        <Tooltip>
-          Copy to your clipboard.
+  const toolbar =
+    (
+      <div className="copytext">
+        {document.queryCommandSupported('copy') &&
+          <OverlayTrigger
+            placement={"top"}
+            overlay={
+              <Tooltip>
+                Copy to your clipboard.
         </Tooltip>
-      }
-    >
-      <Button variant="Light" onClick={(e) => {
-        copyToClipboard(e, 'lol', availabilities, timeZone, messageType, AMPM, MonthDay)}}>Copy</Button>
-    </OverlayTrigger>
-    }
-
-    <OverlayTrigger
-      placement={"top"}
-      overlay={
-        <Tooltip>
-          Made a mess? This cleans it up.
-        </Tooltip>
-      }
-    >
-      <Button variant="Light" onClick={() => {dispatch(clearAvailabilities());}}>Clear</Button> 
-    </OverlayTrigger>
-
-    <OverlayTrigger
-      placement={"top"}
-      overlay={
-        <Tooltip>
-          Don't be rude, send it to them in <strong>their</strong> time zone.
-        </Tooltip>
-      }
-    >
-        <DropdownButton 
-        variant="Light"
-        drop="down"
-        data-flip="false"
-        data-display="static"
-        id="dropdown-button-drop-down" title={moment().tz(timeZone).zoneAbbr()}>
-          {timeZones.sort((a, b) => a > b).map((timeZone) =>
-            <Dropdown.Item
-            data-display="static"
-            data-flip="false"
-            as="a" onClick={() => {setTimeZone(timeZone)}}>
-              {width < 850 ? moment().tz(timeZone).zoneAbbr() : moment().tz(timeZone).zoneAbbr() + ' - ' + timeZone}
-              </Dropdown.Item>
-          )}
-        </DropdownButton>
-    </OverlayTrigger>
-
-      <OverlayTrigger
-        placement={"top"}
-        overlay={
-          <Tooltip>
-            Don't be boring.
-          </Tooltip>
+            }
+          >
+            <Button variant="Light" onClick={(e) => {
+              var xhr = new XMLHttpRequest();
+              xhr.open("GET", "https://api.countapi.xyz/hit/whattime.today/copy");
+              xhr.responseType = "json";
+              xhr.send();
+              copyToClipboard(e, 'lol', availabilities, timeZone, messageType, AMPM, MonthDay)
+            }
+            }>Copy</Button>
+          </OverlayTrigger>
         }
-      >
-        <DropdownButton 
-        variant="Light"
-        drop="down"
-        id="dropdown-button-drop-down" title={messageType}>
-          {messageTypes.map((type) =>
-            <Dropdown.Item
-            as="a" onClick={() => {setMessageType(type)}}>
-              {type}
+
+        <OverlayTrigger
+          placement={"top"}
+          overlay={
+            <Tooltip>
+              Made a mess? This cleans it up.
+        </Tooltip>
+          }
+        >
+          <Button variant="Light" onClick={() => { dispatch(clearAvailabilities()); }}>Clear</Button>
+        </OverlayTrigger>
+
+        <OverlayTrigger
+          placement={"top"}
+          overlay={
+            <Tooltip>
+              Don't be rude, send it to them in <strong>their</strong> time zone.
+        </Tooltip>
+          }
+        >
+          <DropdownButton
+            variant="Light"
+            drop="down"
+            data-flip="false"
+            data-display="static"
+            id="dropdown-button-drop-down" title={moment().tz(timeZone).zoneAbbr()}>
+            {timeZones.sort((a, b) => a > b).map((timeZone) =>
+              <Dropdown.Item
+                data-display="static"
+                data-flip="false"
+                as="a" onClick={() => { setTimeZone(timeZone) }}>
+                {width < 850 ? moment().tz(timeZone).zoneAbbr() : moment().tz(timeZone).zoneAbbr() + ' - ' + timeZone}
               </Dropdown.Item>
-          )}
-        </DropdownButton>
-    </OverlayTrigger>
+            )}
+          </DropdownButton>
+        </OverlayTrigger>
 
-    <OverlayTrigger
-      placement={"top"}
-      overlay={
-        <Tooltip>
-          Not everyone is from America.
+        <OverlayTrigger
+          placement={"top"}
+          overlay={
+            <Tooltip>
+              Don't be boring.
+          </Tooltip>
+          }
+        >
+          <DropdownButton
+            variant="Light"
+            drop="down"
+            id="dropdown-button-drop-down" title={messageType}>
+            {messageTypes.map((type) =>
+              <Dropdown.Item
+                as="a" onClick={() => { setMessageType(type) }}>
+                {type}
+              </Dropdown.Item>
+            )}
+          </DropdownButton>
+        </OverlayTrigger>
+
+        <OverlayTrigger
+          placement={"top"}
+          overlay={
+            <Tooltip>
+              Not everyone is from America.
         </Tooltip>
-      }
-    >
-        <ToggleButtonGroup type="checkbox" defaultValue={1} onChange={handleAMPMChange}>
-          <ToggleButton value={1} variant="Light">AM/PM</ToggleButton>
-        </ToggleButtonGroup>
+          }
+        >
+          <ToggleButtonGroup type="checkbox" defaultValue={1} onChange={handleAMPMChange}>
+            <ToggleButton value={1} variant="Light">AM/PM</ToggleButton>
+          </ToggleButtonGroup>
 
-    </OverlayTrigger>
+        </OverlayTrigger>
 
-    <OverlayTrigger
-      placement={"top"}
-      overlay={
-        <Tooltip>
-          Again, not everyone is from America.
+        <OverlayTrigger
+          placement={"top"}
+          overlay={
+            <Tooltip>
+              Again, not everyone is from America.
         </Tooltip>
-      }
-    >
-        <ToggleButtonGroup type="checkbox" defaultValue={1} onChange={handleMonthDayChange}>
-          <ToggleButton value={1} variant="Light">{
-          width < 600 ? (MonthDay ? "M/D" : "D/M") : (MonthDay ? "Month/Day" : "Day/Month")
-          }</ToggleButton>
-        </ToggleButtonGroup>
+          }
+        >
+          <ToggleButtonGroup type="checkbox" defaultValue={1} onChange={handleMonthDayChange}>
+            <ToggleButton value={1} variant="Light">{
+              width < 600 ? (MonthDay ? "M/D" : "D/M") : (MonthDay ? "Month/Day" : "Day/Month")
+            }</ToggleButton>
+          </ToggleButtonGroup>
 
-    </OverlayTrigger>
+        </OverlayTrigger>
 
-  </div>);
+      </div>);
 
-  const home = (      
+  const home = (
     <div className="Body">
       <div className="Calendar">
-          <MyCalendar initDate={new Date()}/>
+        <MyCalendar initDate={new Date()} />
       </div>
-    <div>
-    <div className="below-calendar">
-      <img className="peep1" src={peep1} />
-      <div id="lol">   
-          {toolbar}
-        <Card className="output-card" classes={{ root: classes.card }} variant="outlined">
-          <List style={{maxHeight: 240, overflow: 'auto'}}>
-              <CardContent>
-              {outputToString(availabilities, timeZone, messageType, AMPM, MonthDay).map((out, i) => {
-                return <p key={i} style={{textAlign: "left", fontSize: 13}}>{out}</p>
-              })}
-              </CardContent>
-          </List>
-        </Card>
-        </div>
-        {
-          // width 
-          <img className="peep2" src={peep2}/>
+      <div>
+        <div className="below-calendar">
+          <img className="peep1" src={peep1} />
+          <div id="lol">
+            {toolbar}
+            <Card className="output-card" classes={{ root: classes.card }} variant="outlined">
+              <List style={{ maxHeight: 240, overflow: 'auto' }}>
+                <CardContent>
+                  {outputToString(availabilities, timeZone, messageType, AMPM, MonthDay).map((out, i) => {
+                    return <p key={i} style={{ textAlign: "left", fontSize: 13 }}>{out}</p>
+                  })}
+                </CardContent>
+              </List>
+            </Card>
+          </div>
+          {
+            // width 
+            <img className="peep2" src={peep2} />
 
-        }
-      </div>
+          }
+        </div>
       </div>
     </div>);
 
-      
+
   return (
     <div className="App">
-    {/* <div className="App-border"> */}
-        <Router>
-          <Header handleSignClick={handleSignClick}/>
-              <Switch>
-                <Route path="/privacy">
-                  {/* <div className="otherbody"> */}
-                    <Privacy/>
-                  {/* </div> */}
-                </Route>
+      {/* <div className="App-border"> */}
+      <Router>
+        <Header handleSignClick={handleSignClick} />
+        <Switch>
+          <Route path="/privacy">
+            {/* <div className="otherbody"> */}
+            <Privacy />
+            {/* </div> */}
+          </Route>
 
-                <Route path="/about">
-                  {/* <div className="otherbody"> */}
-                    <About/>
-                  {/* </div> */}
-                </Route>
+          <Route path="/about">
+            {/* <div className="otherbody"> */}
+            <About />
+            {/* </div> */}
+          </Route>
 
-                <Route path="/">
-                    {home}
-                </Route>
-              </Switch>
-          <Footer/>
-    </Router>
-    {/* </div> */}
-  </div>
+          <Route path="/">
+            {home}
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+      {/* </div> */}
+    </div>
   );
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AppBar } from '@material-ui/core';
 import './Header.css'
 import { Link } from "react-router-dom";
@@ -6,43 +6,60 @@ import { useSelector } from "react-redux";
 import { OverlayTrigger, Tooltip, Button } from 'react-bootstrap';
 
 export default function Header(props) {
+  const [counter, setCounter] = useState(0);
 
-    const user = useSelector(state => (state.user));
+  const user = useSelector(state => (state.user));
 
-    return (
-    <AppBar position="static" style={{background: 'white', color: "black"}}> 
+  useEffect(() => {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.countapi.xyz/get/whattime.today/copy");
+    xhr.responseType = "json";
+    xhr.onload = function () {
+      setCounter(this.response.value);
+    }
+    xhr.send();
+  }, []);
 
-        <div className="titlediv">
 
+  return (
+    <AppBar position="static" style={{ background: 'white', color: "black" }}>
+
+      <div className="titlediv">
+
+        <div className="titledivtitle">
           <Link to="/">
             <h1 className="title">
-            What time today? 
+              What time today?
             </h1>
           </Link>
+          <div>
+            &nbsp;<p>Has helped setup {counter} meetings to date!</p>
+          </div>
+        </div>
 
-            <div className="toolbar-buttons">
+        <div className="toolbar-buttons">
 
-                <h3 className="title">
-                { user ? "Hi, " + user.firstName + "!" : "Hi, Guest!" }
-                </h3>
+          <h3 className="title">
+            {user ? "Hi, " + user.firstName + "!" : "Hi, Guest!"}
+          </h3>
 
-                <OverlayTrigger
-                  placement={"bottom"}
-                    overlay={
-                      <Tooltip 
-                      className="overlay"
-                      style={{zIndex: 3}}>
-                        {user ? "Removes and revokes access to Google Calendar data." : "Imports Google Calendar events to calendar." }
-                        
-                      </Tooltip>
-                    }
-                  >
-                  <Button variant="Light" className="sign-button" onClick={props.handleSignClick}>
-                      {user ? "Google Sign Out" : "Google Sign In" }
-                  </Button>
-                </OverlayTrigger>
-            </div> 
+          <OverlayTrigger
+            placement={"bottom"}
+            overlay={
+              <Tooltip
+                className="overlay"
+                style={{ zIndex: 3 }}>
+                {user ? "Removes and revokes access to Google Calendar data." : "Imports Google Calendar events to calendar."}
+
+              </Tooltip>
+            }
+          >
+            <Button variant="Light" className="sign-button" onClick={props.handleSignClick}>
+              {user ? "Google Sign Out" : "Google Sign In"}
+            </Button>
+          </OverlayTrigger>
+        </div>
       </div>
     </AppBar>
-    );
+  );
 }
