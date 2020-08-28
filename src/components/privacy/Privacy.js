7f1e16aaa68b981576ce81f6735aab1c7e23d1be
Jonathan Cai <caimjonathan@gmail.com>
2020-08-28 12:08:01 -0700

    same ^

--- src/components/privacy/Privacy.js
+++ src/components/privacy/Privacy.js
@@ -6,8 +6,8 @@ export default function Privacy() {
             <div className="privacy-page">
                 <h2>User Data</h2>
                 <p>Security and user data privacy is taken seriously.</p>
-                <p>This application is run strictly client-side (in your browser), and thus, <b>all of the above are not stored in any database.</b></p>
                 <p>Signing in through Google grants us access to your name, your email address, and your Google Calendar data.</p>
+                <p>This application is run strictly client-side (in your browser), and thus, <b>all of the above are not stored in any database.</b></p>
                 <p>If you choose to sign in, your calendars are retrieved through the Google Calendar API and is only used to display on the home page.
                 You may choose to revoke access to your Google Calendar data by signing out, and the data will be removed from your session.</p>
                 <p>Also, the availabities that you select on the calendar only persist for your session and is not stored anywhere.</p>
