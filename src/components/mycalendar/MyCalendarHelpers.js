9796de02c8d7da1877b557e6ad8b0e4c121c9560
Jonathan Cai <caimjonathan@gmail.com>
2021-03-28 01:52:48 -0700

    improve calendar event styling

--- src/components/mycalendar/MyCalendarHelpers.js
+++ src/components/mycalendar/MyCalendarHelpers.js
@@ -1,53 +1,53 @@
-import { addAvailability, removeAvailability } from '../../redux/actions';
+import { addAvailability, removeAvailability } from "../../redux/actions";
 
 // When you select an availability event delete it
 export function onSelectEvent(event, dispatch) {
-    if(event.availability) {
-        dispatch(removeAvailability(event));
-    } else {
-        console.log("Selected event that is not availability, cannot delete");
-    }
-} 
-  
+  if (event.availability) {
+    dispatch(removeAvailability(event));
+  } else {
+    console.log("Selected event that is not availability, cannot delete");
+  }
+}
+
 // When you select an available slot, create the availability, add it to availabilities
 // Also add the availability to the output for the parent component
 export function onSelectAvailableSlot(dispatch, slotInfo) {
-    const newAvailability = {
-        'title': 'Availability 👌 (click to delete)',
-        'start': slotInfo.start,
-        'end': slotInfo.end,
-        'hexColor': "#464646",
-        'visible': true,
-        'availability': true,
-    };
-    dispatch(addAvailability(newAvailability));
+  const newAvailability = {
+    title: "Availability 👌 (click to delete)",
+    start: slotInfo.start,
+    end: slotInfo.end,
+    hexColor: "#464646",
+    visible: true,
+    availability: true,
+  };
+  dispatch(addAvailability(newAvailability));
 }
 
-  
 // Gets the style for each event rendered
 export function eventStyleGetter(event, start, end, isSelected) {
-    let cursor;
-    let color;
+  let cursor;
+  let color;
 
-    if (event.availability) {
-        color = 'white';
-        cursor = 'pointer';
-    } else {
-        color = 'black';
-        cursor = 'auto';
-    }
-    const backgroundColor = event.hexColor;
-    const style = {
-        backgroundColor: backgroundColor,
-        borderRadius: '5px',
-        opacity: 1,
-        color: color,
-        display: 'block',
-        cursor: cursor,
-        visible: false,
-    };
+  if (event.availability) {
+    color = "white";
+    cursor = "pointer";
+  } else {
+    color = "black";
+    cursor = "auto";
+  }
 
-    return {
-        style: style
-    };
+  const backgroundColor = event.hexColor;
+  const style = {
+    backgroundColor: backgroundColor,
+    borderRadius: "5px",
+    opacity: 1,
+    color: "white",
+    display: "block",
+    cursor: cursor,
+    visible: false,
+  };
+
+  return {
+    style: style,
+  };
 }
--- src/util/dateTime.js
+++ src/util/dateTime.js
@@ -1,25 +1,31 @@
-import {messageTypes} from '../App';
- 
+import { messageTypes } from "../App";
+
 // const monthNames = ["January", "February", "March", "April", "May", "June",
 //   "July", "August", "September", "October", "November", "December"];
 
-const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", 
-  "Thursday", "Friday", "Saturday"];
+const dayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
-const moment = require('moment-timezone');
+const moment = require("moment-timezone");
 
 function formatAMPM(date) {
   // https://stackoverflow.com/questions/8888491/how-do-you-display-javascript-datetime-in-12-hour-am-pm-format
   var hours = date.getHours();
   var minutes = date.getMinutes();
-  var ampm = hours >= 12 ? 'pm' : 'am';
+  var ampm = hours >= 12 ? "pm" : "am";
   hours = hours % 12;
   hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? '0'+minutes : minutes;
-  var strTime = hours + ':' + minutes + ' ' + ampm;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  var strTime = hours + ":" + minutes + " " + ampm;
   return strTime;
 }
-
 
 export function outputToString(output, timeZone, messageType, AMPM, MonthDay) {
   // Currently only supporting single day availabilities
@@ -27,7 +33,10 @@ export function outputToString(output, timeZone, messageType, AMPM, MonthDay) {
     return ["Copy not working!! Notify me in the feedback form please!"];
   }
 
-  if (output.length === 0) return ["Nothing selected. Click and drag on the calendar to select availability."];
+  if (output.length === 0)
+    return [
+      "Nothing selected. Click and drag on the calendar to select availability.",
+    ];
 
   // Sort by start time
   var sortedOutput = [...output];
@@ -38,48 +47,72 @@ export function outputToString(output, timeZone, messageType, AMPM, MonthDay) {
   for (let i = 0; i < output.length; i++) {
     let out = sortedOutput[i];
 
-    let start = new Date(out.start.toLocaleString("en-US", {timeZone: timeZone}));
-    let end = new Date(out.end.toLocaleString("en-US", {timeZone: timeZone}));
+    let start = new Date(
+      out.start.toLocaleString("en-US", { timeZone: timeZone })
+    );
+    let end = new Date(out.end.toLocaleString("en-US", { timeZone: timeZone }));
     // var longtz = out.start.toTimeString().match(/\((.+)\)/)[1];
     var shorttz = moment().tz(timeZone).zoneAbbr();
-    
+
     // let month = monthNames[out.start.getMonth()];
     let day = dayNames[start.getDay()];
     let monthNum = start.getMonth() + 1;
     let dayNum = start.getDate();
 
-    let startTime = AMPM ? formatAMPM(start) : start.getHours() + ":" + (start.getMinutes()<10?'0':'') + start.getMinutes() ;
-    let endTime = AMPM ? formatAMPM(end) : end.getHours() + ":" + (end.getMinutes()<10?'0':'') + end.getMinutes() ;
+    let startTime = AMPM
+      ? formatAMPM(start)
+      : start.getHours() +
+        ":" +
+        (start.getMinutes() < 10 ? "0" : "") +
+        start.getMinutes();
+    let endTime = AMPM
+      ? formatAMPM(end)
+      : end.getHours() +
+        ":" +
+        (end.getMinutes() < 10 ? "0" : "") +
+        end.getMinutes();
 
-    let singleResult = MonthDay ? `${day} (${monthNum}/${dayNum}) ${startTime} - ${endTime} ${shorttz}`
-     : `${day} (${dayNum}/${monthNum}) ${startTime} - ${endTime} ${shorttz}`;
+    let singleResult = MonthDay
+      ? `${day} (${monthNum}/${dayNum}) ${startTime} - ${endTime} ${shorttz}`
+      : `${day} (${dayNum}/${monthNum}) ${startTime} - ${endTime} ${shorttz}`;
 
     result.push(singleResult);
   }
-  
-  
-  if (messageTypes[0] === messageType) { // NORMAL
-    result.unshift("I\'m available these times:");
+
+  if (messageTypes[0] === messageType) {
+    // NORMAL
+    result.unshift("I'm available these times:");
   }
-  if (messageTypes[1] === messageType) { // CUTE
-    result.unshift("Can we please do one of these times 🥺👉👈?"); 
+  if (messageTypes[1] === messageType) {
+    // CUTE
+    result.unshift("Can we please do one of these times 🥺👉👈?");
   }
-  if (messageTypes[2] === messageType) { // AGGRESSIVE
-    result.unshift("This is the last time I'm sending you my availability 😡"); 
+  if (messageTypes[2] === messageType) {
+    // AGGRESSIVE
+    result.unshift("This is the last time I'm sending you my availability 😡");
   }
-  if (messageTypes[3] === messageType) { // ELON
-    result = ["I don't have time."]
+  if (messageTypes[3] === messageType) {
+    // ELON
+    result = ["I don't have time."];
   }
-  if (messageTypes[4] === messageType) { // RAW
+  if (messageTypes[4] === messageType) {
+    // RAW
   }
-  if (messageTypes[5] === messageType) { // Inverse
-    result.unshift("I cannot do these times:"); 
+  if (messageTypes[5] === messageType) {
+    // Inverse
+    result.unshift("I cannot do these times:");
   }
-  
+
   return result;
 }
 
-export function outputToStringCopy(output, timeZone, messageType, AMPM, MonthDay) {
+export function outputToStringCopy(
+  output,
+  timeZone,
+  messageType,
+  AMPM,
+  MonthDay
+) {
   var out = outputToString(output, timeZone, messageType, AMPM, MonthDay);
   var result = out.join("\r\n");
   return result;
