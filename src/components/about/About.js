9aebcc64afe8826c7e93ae2bb6b3d26a85462770
Jonathan Cai <caimjonathan@gmail.com>
2021-03-29 12:12:14 -0700

    about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -17,54 +17,12 @@ export default function About() {
     <div className="not-homepage">
       <div className="aboutpage">
         <p>
-          Thanks to{" "}
-          <a
-            style={{ color: "black" }}
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.instagram.com/pablostanley/"
-          >
-            Pablo Stanley
-          </a>{" "}
-          for{" "}
-          <a
-            style={{ color: "black" }}
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.instagram.com/pablostanley/"
-          >
-            Open Peeps
-          </a>
-          .{" "}
-        </p>
-        <p>
-          Thanks to{" "}
-          <a
-            style={{ color: "black" }}
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://github.com/jquense"
-          >
-            jquense
-          </a>{" "}
-          for{" "}
-          <a
-            style={{ color: "black" }}
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.npmjs.com/package/react-big-calendar/"
-          >
-            react-big-calendar
-          </a>
-          .
-        </p>
-        <p>
           Made by{" "}
           <a
             style={{ color: "black" }}
             target="_blank"
             rel="noopener noreferrer"
-            href="https://jonathancai.com/"
+            href="https://twitter.com/jonathanmcai"
           >
             Jonathan Cai
           </a>
